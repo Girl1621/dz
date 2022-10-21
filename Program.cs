1f0ab3b053c9f753b7c78f66673c99d848c14e46
Ksenia <115340620+Girl1621@users.noreply.github.com>
2022-10-21 19:42:15 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+string number = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine("вторая цифра этого числа:"+number[1]);
+
+// Напишите программу, которая выводит третью цифру заданного числа
+//или сообщает, что третьей цифры нет.
+
+Console.Write("Введите трехзначное число: ");
+
+int randomNumber = Convert.ToInt32(Console.ReadLine());
+
+string number = Convert.ToString(randomNumber); 
+
+if (number.Length > 2)
+{
+    Console.WriteLine("третье число этого числа:"+number[2]);
+}
+
+else
+ {
+  Console.WriteLine("третьей цифры нет");
+ }
+
+
+ //Напишите программу, которая принимает на вход цифру,обозначающую
+ //день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
+ {
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine($"{dayNumber} да");
+  }
+  
+  else Console.WriteLine($"{dayNumber} нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
